dda7f3dd13dec92bef39ccbc92760fbd3cce0f40
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-09-10 19:34:48 -0700

    Ignore 'js' directory

--- js/views/StaticRenderer.js
+++
@@ -1,9 +0,0 @@
-var StaticRenderer;
-
-StaticRenderer = require("StaticRenderer");
-
-StaticRenderer.displayName = "StaticRenderer";
-
-module.exports = require("ReactElement").createFactory(StaticRenderer);
-
-//# sourceMappingURL=map/StaticRenderer.map
